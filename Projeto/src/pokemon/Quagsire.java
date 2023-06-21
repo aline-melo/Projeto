696f91fd0dad6c6e4da7227698ecc099dfb2897b
aline-melo <aline1amo@gmail.com>
2023-06-21 15:18:00 -0300

    agora é sobre os pokemons iniciais

--- Projeto/src/pokemon/Quagsire.java
+++ Projeto/src/pokemon/Quagsire.java
@@ -7,50 +7,52 @@ import java.awt.event.*;
 
 public class Quagsire implements ActionListener{
 	 
+	//criando os elementos que serão usados na nossa janela
 	JFrame janela = new JFrame("pokemon");
-	JButton botaoWooper = new JButton("wooper"); 
+	JButton botaosquirtle = new JButton("squirtle"); 
 	JButton botaoBulbassauro = new JButton("bulbassauro"); 
-	JButton botaoQuagsire = new JButton("quagsire"); 
+	JButton botaocharmander = new JButton("charmander"); 
 	JLabel textoAExibir = new JLabel("Pressione um botão."); 
-	JLabel imagemAExibir = new JLabel(new ImageIcon());
+	JLabel imagemAExibir = new JLabel(new ImageIcon("src\\files\\pokebola.png"));
 
 	public Quagsire(){
 		//configura a nossa janela
 		janela.setLayout(new FlowLayout()); 	//estaremos usando o FlowLayout
 		janela.setSize(420, 600); //configura o tamanho
-		janela.setVisible(true); //coloca ela como visível
 
 		// Adiciona os botões, o texto e a imagem na janela. 
-		janela.add(botaoWooper);
+		janela.add(botaosquirtle);
 		janela.add(botaoBulbassauro);
-		janela.add(botaoQuagsire);
+		janela.add(botaocharmander);
 
 		janela.add(textoAExibir); 
 
 		janela.add(imagemAExibir);
 		
-		// Adiciona eventos. 
-		botaoWooper.addActionListener(this);
+		// Adiciona eventos (recebe um sinal caso o botão seja clicado
+		botaosquirtle.addActionListener(this);
 		botaoBulbassauro.addActionListener(this);
-		botaoQuagsire.addActionListener(this);
+		botaocharmander.addActionListener(this);
 		
-		// Mostra o frame construído.
+		// Mostra a janela que construímos
 		janela.setVisible(true); 
 	}
 
- 	//Método para gerência de eventos
+ 	//Método para gerência de eventos (lógica: se isto acontecer, faça tal ação)
 	public void actionPerformed(ActionEvent evento) {
-		if(evento.getActionCommand().contains("wooper"))  {
-			textoAExibir.setText("wooper "); 
-			imagemAExibir.setIcon(new ImageIcon("src\\files\\wooper.jpg"));
+		if(evento.getActionCommand().contains("squirtle"))  {
+			//muda o texto de "pressione um botão" para "squirtle"
+			textoAExibir.setText("squirtle "); 
+			
+			imagemAExibir.setIcon(new ImageIcon("src\\files\\squirtle.gif"));
 		}			
 		if(evento.getActionCommand().equals("bulbassauro"))  {
 			textoAExibir.setText("bulbassauro"); 
-			imagemAExibir.setIcon(new ImageIcon("src\\files\\bulbassauro.jpg"));
+			imagemAExibir.setIcon(new ImageIcon("src\\files\\bulbassauro.gif"));
 		}
-		if(evento.getActionCommand().equals("quagsire"))  {
-			textoAExibir.setText("quagsire"); 
-			imagemAExibir.setIcon(new ImageIcon("src\\files\\quagsire.jpg"));
+		if(evento.getActionCommand().equals("charmander"))  {
+			textoAExibir.setText("charmander"); 
+			imagemAExibir.setIcon(new ImageIcon("src\\files\\charmander.gif"));
 		}
 	} 
 	
